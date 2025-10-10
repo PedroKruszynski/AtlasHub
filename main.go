5e49f28639d51b639a5787757b58e48486e76e85
pedro <pedrokruszynski@gmail.com>
2025-10-10 00:55:41 -0300

    feat: talvez tela de descanso

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"atlasHub/internal/usdbrl"
 	homeBackground "atlasHub/static/home"
 	"fmt"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -57,14 +58,28 @@ func main() {
 		layout.NewSpacer(),
 	)
 
-	stack := container.NewStack()
-	homeLayer := homeContent
-	tela1Layer := tela1Content
+	descansoBtn := widget.NewButton("Clique para voltar ao menu", nil)
+	descansoContent := container.NewStack(descansoBtn)
 
-	stack.Add(homeLayer)
+	stack := container.NewStack(homeContent, tela1Content, descansoContent)
+	currentScreen := homeContent
+	stack.Objects = []fyne.CanvasObject{currentScreen}
+
+	inactivity := 3 * time.Second
+	timer := time.NewTimer(inactivity)
+
+	resetTimer := func() {
+		if !timer.Stop() {
+			select {
+			case <-timer.C:
+			default:
+			}
+		}
+		timer.Reset(inactivity)
+	}
 
 	btn1.OnTapped = func() {
-		// busca cotação
+		resetTimer()
 		cot, err := usdbrl.FetchDollar()
 		var texto string
 		if err != nil {
@@ -73,13 +88,35 @@ func main() {
 			texto = fmt.Sprintf("USD = R$ %.2f", cot)
 		}
 		tela1Label.SetText(texto)
-		stack.Objects = []fyne.CanvasObject{tela1Layer}
+		currentScreen = tela1Content
+		stack.Objects = []fyne.CanvasObject{tela1Content}
 		stack.Refresh()
 	}
+
 	btnVoltar.OnTapped = func() {
-		stack.Objects = []fyne.CanvasObject{homeLayer}
+		resetTimer()
+		currentScreen = homeContent
+		stack.Objects = []fyne.CanvasObject{homeContent}
 		stack.Refresh()
 	}
+
+	btn2.OnTapped = func() { resetTimer() }
+	btn3.OnTapped = func() { resetTimer() }
+
+	descansoBtn.OnTapped = func() {
+		resetTimer()
+		currentScreen = homeContent
+		stack.Objects = []fyne.CanvasObject{homeContent}
+		stack.Refresh()
+	}
+
+	go func() {
+		for {
+			<-timer.C
+			stack.Objects = []fyne.CanvasObject{descansoContent}
+			stack.Refresh()
+		}
+	}()
 
 	myWindow.SetContent(stack)
 
