e2205c4cab9c77e444c4d922dff1b8ae0b156732
pedro <pedrokruszynski@gmail.com>
2025-10-14 00:34:20 -0300

    feat: invisible screensaver

--- internal/screens/screensaver/screensaver.go
+++ internal/screens/screensaver/screensaver.go
@@ -1,22 +1,60 @@
 package screensaver
 
 import (
+	homeBackground "atlasHub/static/home"
 	"time"
 
 	"fyne.io/fyne/v2"
 
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
-func ScreensaverContainer(onClick func()) fyne.CanvasObject {
-	btn := widget.NewButton("Clique para voltar ao menu", func() {
-		if onClick != nil {
-			onClick()
-		}
-	})
+type invisibleClickable struct {
+	widget.BaseWidget
+	onTap func()
+}
 
-	return container.NewStack(btn)
+func NewInvisibleClickable(onTap func()) *invisibleClickable {
+	w := &invisibleClickable{onTap: onTap}
+	w.ExtendBaseWidget(w)
+	return w
+}
+
+func (w *invisibleClickable) Tapped(_ *fyne.PointEvent) {
+	if w.onTap != nil {
+		w.onTap()
+	}
+}
+
+func (w *invisibleClickable) TappedSecondary(_ *fyne.PointEvent) {}
+
+func (w *invisibleClickable) CreateRenderer() fyne.WidgetRenderer {
+	return &invisibleRenderer{w: w}
+}
+
+type invisibleRenderer struct {
+	w *invisibleClickable
+}
+
+func (r *invisibleRenderer) Layout(size fyne.Size) {}
+func (r *invisibleRenderer) MinSize() fyne.Size    { return fyne.NewSize(0, 0) }
+func (r *invisibleRenderer) Refresh()              {}
+
+func (r *invisibleRenderer) Objects() []fyne.CanvasObject { return nil }
+func (r *invisibleRenderer) Destroy()                     {}
+
+func ScreensaverContainer(onClick func()) fyne.CanvasObject {
+	img := canvas.NewImageFromResource(homeBackground.BackgroundPng)
+	img.FillMode = canvas.ImageFillStretch
+
+	clickable := NewInvisibleClickable(onClick)
+
+	return container.NewStack(
+		img,
+		clickable,
+	)
 }
 
 func StartScreensaverTimer(inactivity time.Duration, onTimeout func()) func() {
